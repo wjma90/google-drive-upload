2ad4a49b0103be2d3fb0002c1e33ce4e52b42933
Dhiva Hariharan <dhiva.hariharan@autodata-group.com>
2016-03-18 00:32:10 +0000

    updated upload script to add create directory option and upload entire directory.

--- upload.sh
+++ upload.sh
@@ -5,39 +5,171 @@
 
 #!/bin/bashset -e
 
-#TODO
-# 1. Create directory option
-# 2. Upload entire directory
-# 3. Resume failed upload
-
-#Simple validation to check whether the given folder exists
-if [ ! -f $1 ]
-then
-        echo "please provide a file in arg"
-        exit -1
-fi
-
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-FILE=$1
 #Configuration variables
+
+function usage(){
+
+	echo -e "\nThe script can be used to upload file/directory to google drive." 
+	echo -e "\nUsage:\n $0 [options..] <filename> <foldername> \n"
+	echo -e "Foldername argument is optional. If not provided, the file will be uploaded to preconfigured google drive. \n"
+	echo -e "File name argument is optional if create directory option is used. \n"
+	echo -e "Options:\n"
+	echo -e "-C | --create-dir <foldername> - option to create directory. Will provide folder id."
+	echo -e "-r | --root-dir <google_folderid> - google folder id to which the file/directory to upload."
+	echo -e "-v | --verbose - Display detailed message."
+	echo -e "-h | --help - Display usage instructions.\n" 
+	exit 0;
+}
 ROOT_FOLDER=""
 CLIENT_ID=""
 CLIENT_SECRET=""
 REFRESH_TOKEN=""
 SCOPE=${SCOPE:-"https://docs.google.com/feeds"}
 
-# Method to extract data from json response
-function jsonValue() {
-KEY=$1
-num=$2
-awk -F"[,:}][^:\/\/]" '{for(i=1;i<=NF;i++){if($i~/\042'$KEY'\042/){print $(i+1)}}}' | tr -d '"' | sed -n ${num}p | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e 's/[,]*$//'
-}
+#Internal variable
+ACCESS_TOKEN=""
+FILE=""
+FOLDERNAME=""
+curl_args=""
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [ -e $HOME/.googledrive.conf ]
 then
     . $HOME/.googledrive.conf
 fi
+
+PROGNAME=${0##*/}
+SHORTOPTS="vhr:C:" 
+LONGOPTS="verbose,help,create-dir:,root-dir:" 
+
+set -o errexit -o noclobber -o pipefail #-o nounset 
+OPTS=$(getopt -s bash --options $SHORTOPTS --longoptions $LONGOPTS --name $PROGNAME -- "$@" ) 
+
+# script to parse the input arguments
+#if [ $? != 0 ] ; then echo "Failed parsing options." >&2 ; exit 1 ; fi
+
+eval set -- "$OPTS"
+
+VERBOSE=false
+HELP=false
+
+while true; do
+  case "$1" in
+    -v | --verbose ) VERBOSE=true;curl_args="--progress"; shift ;;
+    -h | --help )    usage; shift ;;
+    -C | --create-dir ) FOLDERNAME="$2"; shift 2 ;;
+    -r | --root-dir ) ROOT_FOLDER="$2"; shift 2 ;;
+    -- ) shift; break ;;
+    * )  break ;;
+  esac
+done
+
+if [ ! -z "$1" ]
+then
+	FILE=$1
+fi
+
+if [ ! -z "$2" ] && [ -z "$FOLDERNAME" ]
+then
+	FOLDERNAME=$2
+fi
+
+if [ $# = 0 ] || [ -z "$FOLDERNAME" ]
+	then
+		usage
+fi
+
+# Method to extract data from json response
+function jsonValue() {
+KEY=$1
+num=$2
+awk -F"[,:}][^:\/\/]" '{for(i=1;i<=NF;i++){if($i~/\042'$KEY'\042/){print $(i+1)}}}' | tr -d '"' | sed -n ${num}p | sed -e 's/[}]*$//' -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e 's/[,]*$//' 
+}
+
+function log() {
+	
+	if [ "$VERBOSE" = true ]; then
+		echo -e "${1}"
+	
+	fi
+}
+
+function createDirectory(){
+	DIRNAME="$1"
+	ROOTDIR="$2"
+	ACCESS_TOKEN="$3"
+	FOLDER_ID=""
+    QUERY="mimeType='application/vnd.google-apps.folder' and title='$DIRNAME'"
+    QUERY=$(echo $QUERY | sed -f ${DIR}/url_escape.sed)
+
+	SEARCH_RESPONSE=`/usr/bin/curl \
+					--silent \
+					-XGET \
+					-H "Authorization: Bearer ${ACCESS_TOKEN}" \
+					 "https://www.googleapis.com/drive/v2/files/${ROOTDIR}/children?orderBy=title&q=${QUERY}&fields=items%2Fid"`
+
+	FOLDER_ID=`echo $SEARCH_RESPONSE | jsonValue id`
+
+
+	if [ -z "$FOLDER_ID" ]
+	then
+		CREATE_FOLDER_POST_DATA="{\"mimeType\": \"application/vnd.google-apps.folder\",\"title\": \"$DIRNAME\",\"parents\": [{\"id\": \"$ROOTDIR\"}]}"
+		CREATE_FOLDER_RESPONSE=`/usr/bin/curl \
+								--silent  \
+								-X POST \
+								-H "Authorization: Bearer ${ACCESS_TOKEN}" \
+								-H "Content-Type: application/json; charset=UTF-8" \
+								-d "$CREATE_FOLDER_POST_DATA" \
+								"https://www.googleapis.com/drive/v2/files?fields=id"`
+		FOLDER_ID=`echo $CREATE_FOLDER_RESPONSE | jsonValue id`
+
+	fi
+	echo "$FOLDER_ID"
+}
+
+function uploadFile(){
+
+	FILE="$1"
+	FOLDER_ID="$2"
+	ACCESS_TOKEN="$3"
+	MIME_TYPE=`file --brief --mime-type "$FILE"`
+	SLUG=`basename "$FILE"`
+	FILESIZE=$(stat -c%s "$FILE")
+
+	# JSON post data to specify the file name and folder under while the file to be created
+	postData="{\"mimeType\": \"$MIME_TYPE\",\"title\": \"$SLUG\",\"parents\": [{\"id\": \"$FOLDER_ID\"}]}"
+	postDataSize=$(echo $postData | wc -c)
+
+	# Curl command to initiate resumable upload session and grab the location URL
+	log "Generating upload link for file $FILE ..."
+	uploadlink=`/usr/bin/curl \
+				--silent \
+				-X POST \
+				-H "Host: www.googleapis.com" \
+				-H "Authorization: Bearer ${ACCESS_TOKEN}" \
+				-H "Content-Type: application/json; charset=UTF-8" \
+				-H "X-Upload-Content-Type: $MIME_TYPE" \
+				-H "X-Upload-Content-Length: $FILESIZE" \
+				-d "$postData" \
+				"https://www.googleapis.com/upload/drive/v2/files?uploadType=resumable" \
+				--dump-header - | sed -ne s/"Location: "//p | tr -d '\r\n'`
+
+	# Curl command to push the file to google drive.
+	# If the file size is large then the content can be split to chunks and uploaded.
+	# In that case content range needs to be specified.
+	log "Uploading file $FILE to google drive..."
+	curl \
+	-X PUT \
+	-H "Authorization: Bearer ${ACCESS_TOKEN}" \
+	-H "Content-Type: $MIME_TYPE" \
+	-H "Content-Length: $FILESIZE" \
+	-H "Slug: $SLUG" \
+	--data-binary "@$FILE" \
+	--output /dev/null \
+	"$uploadlink" \
+	$curl_args
+}
 
 old_umask=`umask`
 umask 0077
@@ -97,72 +229,29 @@ if [ -z "$ACCESS_TOKEN" ]
 	ACCESS_TOKEN=`echo $RESPONSE | jsonValue access_token`
 fi
 # Check to find whether the folder exists in google drive. If not then the folder is created in google drive under the configured root folder
-if [ -z "$2" ] || [ `echo $2 | tr [:upper:] [:lower:]` = `echo "root" | tr [:upper:] [:lower:]` ]
+
+if [ -z "$FOLDERNAME" ] || [[ `echo "$FOLDERNAME" | tr [:upper:] [:lower:]` = `echo "root" | tr [:upper:] [:lower:]` ]]
 	then
-	if [ `echo $2 | tr [:upper:] [:lower:]` = `echo "root" | tr [:upper:] [:lower:]` ]
+	if [[ `echo "$FOLDERNAME" | tr [:upper:] [:lower:]` = `echo "root" | tr [:upper:] [:lower:]` ]]
 	then
 		ROOT_FOLDER="root"
 	fi
     FOLDER_ID=$ROOT_FOLDER
 else
-        FOLDERNAME="$2"
-        QUERY="mimeType='application/vnd.google-apps.folder' and title='$FOLDERNAME'"
-        QUERY=$(echo $QUERY | sed -f ${DIR}/url_escape.sed)
-		SEARCH_RESPONSE=`/usr/bin/curl \
-						--silent \
-						-XGET \
-						-H "Authorization: Bearer ${ACCESS_TOKEN}" \
-						 "https://www.googleapis.com/drive/v2/files/${ROOT_FOLDER}/children?orderBy=title&q=${QUERY}&fields=items%2Fid"`
-		FOLDER_ID=`echo $SEARCH_RESPONSE | jsonValue id`
-		if [ -z "$FOLDER_ID" ]
-		then
-			CREATE_FOLDER_POST_DATA="{\"mimeType\": \"application/vnd.google-apps.folder\",\"title\": \"$FOLDERNAME\",\"parents\": [{\"id\": \"$ROOT_FOLDER\"}]}"
-			CREATE_FOLDER_RESPONSE=`/usr/bin/curl \
-									--silent  \
-									-X POST \
-									-H "Authorization: Bearer ${ACCESS_TOKEN}" \
-									-H "Content-Type: application/json; charset=UTF-8" \
-									-d "$CREATE_FOLDER_POST_DATA" \
-									"https://www.googleapis.com/drive/v2/files?fields=id" `
-			FOLDER_ID=`echo $CREATE_FOLDER_RESPONSE | jsonValue id`
-		fi
+	FOLDER_ID=`createDirectory "$FOLDERNAME" "$ROOT_FOLDER" "$ACCESS_TOKEN"`
 						 
 fi
-FOLDER_ID="$(echo $FOLDER_ID | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
-BOUNDARY=`cat /dev/urandom | head -c 16 | xxd -ps`
-MIME_TYPE=`file --brief --mime-type "$FILE"`
-SLUG=`basename "$FILE"`
-FILESIZE=$(stat -c%s "$FILE")
+	log "Folder ID for folder name $FOLDERNAME : $FOLDER_ID"
 
-# JSON post data to specify the file name and folder under while the file to be created
-postData="{\"mimeType\": \"$MIME_TYPE\",\"title\": \"$SLUG\",\"parents\": [{\"id\": \"$FOLDER_ID\"}]}"
-postDataSize=$(echo $postData | wc -c)
-
-# Curl command to initiate resumable upload session and grab the location URL
-echo "Generating upload link..."
-uploadlink=`/usr/bin/curl \
-			--silent \
-			-X POST \
-			-H "Host: www.googleapis.com" \
-			-H "Authorization: Bearer ${ACCESS_TOKEN}" \
-			-H "Content-Type: application/json; charset=UTF-8" \
-			-H "X-Upload-Content-Type: $MIME_TYPE" \
-			-H "X-Upload-Content-Length: $FILESIZE" \
-			-d "$postData" \
-			"https://www.googleapis.com/upload/drive/v2/files?uploadType=resumable" \
-			--dump-header - | sed -ne s/"Location: "//p | tr -d '\r\n'`
-
-# Curl command to push the file to google drive.
-# If the file size is large then the content can be split to chunks and uploaded.
-# In that case content range needs to be specified.
-echo "Uploading file to google drive..."
-curl \
--X PUT \
--H "Authorization: Bearer ${ACCESS_TOKEN}" \
--H "Content-Type: $MIME_TYPE" \
--H "Content-Length: $FILESIZE" \
--H "Slug: $SLUG" \
---data-binary "@$FILE" \
---output /dev/null \
---progress-bar \
-"$uploadlink"
+if [ ! -z "$FILE" ]; then
+	if [ -f "$FILE" ];then
+		uploadFile "$FILE" "$FOLDER_ID" "$ACCESS_TOKEN"
+	elif [ -d "$FILE" ];then
+			FOLDERNAME=$(basename $FILE)
+			FOLDER_ID=`createDirectory "$FOLDERNAME" "$ROOT_FOLDER" "$ACCESS_TOKEN"`
+			for file in $(find "$FILE" -type f);
+			do
+				uploadFile "$file" "$FOLDER_ID" "$ACCESS_TOKEN"
+			done
+	fi
+fi
